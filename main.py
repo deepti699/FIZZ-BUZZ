44c12c013d0f719f480f6458be34e9a8f559f9f9
DEEPTI BS <deeptibalgar@gmail.com>
2026-02-24 21:40:57 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-
+for number in range(1,100):
+  
+  if number % 3 == 0:
+    
+    print("fizz")
+    
+  elif number % 5 == 0:
+    
+    print("fizz")
+    
+  elif number % 3 and 5 == 0:
+    
+    print("fizzbuzz")
+    
+  else:
+    
+    print(number)
+ 
